d529eed74069b7bdb06f17dca86580627c221638
Bartosz Chaber <bartosz.chaber@ee.pw.edu.pl>
2020-02-13 22:31:12 +0100

    Working neural network for OR gate

--- perceptron.jl
+++ perceptron.jl
@@ -2,90 +2,74 @@ import Random
 Random.seed!(0)
 using LinearAlgebra
 
+function pprint(A)
+  io = IOContext(stdout)
+  io = IOContext(io, :limit=>true)
+  show(io, "text/plain", A)
+  println()
+end
+
 ∑ = sum
 sigmoid(x :: Real) = one(x) / (one(x) + exp(-x))
 linear(x :: Real) = x
 σ = sigmoid
 
-η = 0.95
+η = 0.5
 epochs = 10000
 
-inputs  = 3
-neurons = 2
-outputs = 2
+inputs  = 2
+neurons = 5
+outputs = 1
 
 bₕ = hidden_bias = zeros(1, neurons)
 bₒ = output_bias = zeros(1, outputs)
-bₕ = [0.5 0.5]
-bₒ = [0.5 0.5]
 wₕ = hidden_layer = rand(inputs,  neurons)
 wₒ = output_layer = rand(neurons, outputs)
-wₕ = [0.1 0.2;
-      0.3 0.4;
-      0.5 0.6]
-wₒ = [0.7 0.8;
-      0.9 0.1]
-inputs =  [1 4 5]
-targets = [0.1 0.05]
-
+inputs =  [0 0;
+           0 1;
+           1 0;
+           1 1]
+targets = [0; 1; 1; 0]
 for i=1:epochs
   global wₕ, wₒ, bₕ, bₒ
+#print("wₕ = "); pprint(wₕ)
+#print("wₒ = "); pprint(wₒ)
+  #j = (i-1) % 4 + 1
   j = rand(1:size(inputs, 1))
   x = reshape( inputs[j,:], 1, :)
   y = reshape(targets[j,:], 1, :)
-  #@show x
   ### Feed forward ###
   x̄ = ∑(x * wₕ; dims=1) .+ bₕ
   x̂ = σ.(x̄)
-  #@show x̄
-  #@show x̂
-
+  
   ȳ = ∑(x̂ * wₒ; dims=1) .+ bₒ
   ŷ = σ.(ȳ)
-  #@show ȳ
-  #@show ŷ
-
+  
   ε = 0.5*(y - ŷ).^2
-  E = loss = ∑(ε)
-  @show loss
+  E = ∑(ε)
 
   ### Back propagation ###
-  #@show size(x)
-  #@show size(x̂)
-  #@show size(ŷ)
   ∂ȳ_∂bₒ = 1.
   ∂ȳ_∂wₒ = reshape(x̂, :, 1)
   ∂ŷ_∂ȳ  = ŷ .* (1 .- ŷ)
   ∂E_∂ŷ  = -(y - ŷ)
-  #@show ∂ȳ_∂wₒ
-  #@show ∂ŷ_∂ȳ
-  #@show ∂E_∂ŷ
-  #@show size(wₒ)
-  #@show size(bₒ)
   ∂E_∂wₒ = ∂ȳ_∂wₒ .* ∂ŷ_∂ȳ .* ∂E_∂ŷ 
   ∂E_∂bₒ = ∂ȳ_∂bₒ .* ∂ŷ_∂ȳ .* ∂E_∂ŷ 
-  #@show ∂E_∂wₒ
-  #@show size(∂E_∂wₒ)
-  #@show size(∂E_∂bₒ)
-  wₒ    -= η * ∂E_∂wₒ
-  bₒ    -=2η * ∂E_∂bₒ
   #println("========")
   ∂ȳ_∂x̂  = wₒ
-  ∂ŷ_∂ȳ  = ŷ .* (1 .- ŷ)
-  ∂E_∂x̂  = ∑(∂ŷ_∂ȳ .* ∂ȳ_∂x̂)
   ∂x̂_∂x̄  = x̂ .* (1 .- x̂)
   ∂x̄_∂bₕ = 1.
   ∂x̄_∂wₕ = reshape(x, :, 1)
-  #@show size(∂ȳ_∂x̂)
-  #@show size(∂ŷ_∂ȳ)
-  #@show size(∂x̂_∂x̄)
-  #@show size(∂x̄_∂wₕ)
-  #@show size(wₕ)
-  #@show size(bₕ)
-  ∂E_∂wₕ = ∂x̄_∂wₕ * ∂x̂_∂x̄ .* ∂E_∂x̂
-  ∂E_∂bₕ = ∂x̄_∂bₕ * ∂x̂_∂x̄ .* ∂E_∂x̂
-  #@show size(∂E_∂wₕ)
-  #@show size(∂E_∂bₕ)
+  ∂E_∂x̂ = ∂ȳ_∂x̂ .* ∂ŷ_∂ȳ .* ∂E_∂ŷ
+  ∂E_∂x̄ = ∂x̂_∂x̄ .* ∂E_∂x̂'
+  ∂E_∂wₕ = ∂x̄_∂wₕ * ∂E_∂x̄
+  ∂E_∂bₕ = ∂x̄_∂bₕ * ∂E_∂x̄
+
+  wₒ    -= η * ∂E_∂wₒ
+  bₒ    -= η * ∂E_∂bₒ
   wₕ    -= η * ∂E_∂wₕ
-  bₕ    -=2η * ∂E_∂bₕ
+  bₕ    -= η * ∂E_∂bₕ
+  q₁ = norm(η * ∂E_∂wₒ)
+  q₂ = norm(η * ∂E_∂wₕ)
+  println("$i\t$E\t$q₁\t$q₂")#@show loss
 end
