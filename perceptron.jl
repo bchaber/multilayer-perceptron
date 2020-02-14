c54e8070a26d97c4af4028c8324af8f04cc89430
Bartek Chaber <bartosz.chaber@ee.pw.edu.pl>
2020-02-14 09:09:30 +0100

    Fix calculating dE_dxhat for multiple output neurons

--- perceptron.jl
+++ perceptron.jl
@@ -1,43 +1,29 @@
 import Random
 Random.seed!(0)
-using LinearAlgebra
 
-function pprint(A)
-  io = IOContext(stdout)
-  io = IOContext(io, :limit=>true)
-  show(io, "text/plain", A)
-  println()
-end
-
-∑ = sum
 sigmoid(x :: Real) = one(x) / (one(x) + exp(-x))
-linear(x :: Real) = x
+linear(x :: Real)  = x
 σ = sigmoid
+∑ = sum
 
-η = 0.5
+η = 0.95 # learning rate
 epochs = 10000
 
-inputs  = 2
-neurons = 5
-outputs = 1
+input_neurons  = 4
+hidden_neurons = 5
+output_neurons = 3
 
-bₕ = hidden_bias = zeros(1, neurons)
-bₒ = output_bias = zeros(1, outputs)
-wₕ = hidden_layer = rand(inputs,  neurons)
-wₒ = output_layer = rand(neurons, outputs)
-inputs =  [0 0;
-           0 1;
-           1 0;
-           1 1]
-targets = [0; 1; 1; 0]
+bₕ = hidden_bias = zeros(1, hidden_neurons)
+bₒ = output_bias = zeros(1, output_neurons)
+wₕ = hidden_layer = rand(input_neurons,  hidden_neurons) .- 0.5
+wₒ = output_layer = rand(hidden_neurons, output_neurons) .- 0.5
+include("iris.jl")
 for i=1:epochs
   global wₕ, wₒ, bₕ, bₒ
-#print("wₕ = "); pprint(wₕ)
-#print("wₒ = "); pprint(wₒ)
-  #j = (i-1) % 4 + 1
   j = rand(1:size(inputs, 1))
   x = reshape( inputs[j,:], 1, :)
   y = reshape(targets[j,:], 1, :)
+
   ### Feed forward ###
   x̄ = ∑(x * wₕ; dims=1) .+ bₕ
   x̂ = σ.(x̄)
@@ -53,23 +39,20 @@ for i=1:epochs
   ∂ȳ_∂wₒ = reshape(x̂, :, 1)
   ∂ŷ_∂ȳ  = ŷ .* (1 .- ŷ)
   ∂E_∂ŷ  = -(y - ŷ)
-  ∂E_∂wₒ = ∂ȳ_∂wₒ .* ∂ŷ_∂ȳ .* ∂E_∂ŷ 
-  ∂E_∂bₒ = ∂ȳ_∂bₒ .* ∂ŷ_∂ȳ .* ∂E_∂ŷ 
-  #println("========")
+  ∂E_∂wₒ = ∂ȳ_∂wₒ .* ∂ŷ_∂ȳ .* ∂E_∂ŷ
+  ∂E_∂bₒ = ∂ȳ_∂bₒ .* ∂ŷ_∂ȳ .* ∂E_∂ŷ
+
   ∂ȳ_∂x̂  = wₒ
   ∂x̂_∂x̄  = x̂ .* (1 .- x̂)
   ∂x̄_∂bₕ = 1.
   ∂x̄_∂wₕ = reshape(x, :, 1)
-  ∂E_∂x̂ = ∂ȳ_∂x̂ .* ∂ŷ_∂ȳ .* ∂E_∂ŷ
-  ∂E_∂x̄ = ∂x̂_∂x̄ .* ∂E_∂x̂'
+  ∂E_∂x̂  = ∑(∂ȳ_∂x̂ .* ∂ŷ_∂ȳ .* ∂E_∂ŷ; dims=2)'
+  ∂E_∂x̄  = ∂x̂_∂x̄ .* ∂E_∂x̂
   ∂E_∂wₕ = ∂x̄_∂wₕ * ∂E_∂x̄
   ∂E_∂bₕ = ∂x̄_∂bₕ * ∂E_∂x̄
-
   wₒ    -= η * ∂E_∂wₒ
   bₒ    -= η * ∂E_∂bₒ
   wₕ    -= η * ∂E_∂wₕ
   bₕ    -= η * ∂E_∂bₕ
-  q₁ = norm(η * ∂E_∂wₒ)
-  q₂ = norm(η * ∂E_∂wₕ)
-  println("$i\t$E\t$q₁\t$q₂")#@show loss
+  println(i, "\t", E)
 end
