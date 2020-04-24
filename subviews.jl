a8abbc564498bc3517f8aa5b8d203dcdbdd8a290
Bartosz Chaber <bartosz.chaber@ee.pw.edu.pl>
2020-04-24 20:54:25 +0200

    Reshape subviews according to the provided size tuple

--- subviews.jl
+++ subviews.jl
@@ -1,8 +1,9 @@
 function subviews(u, ns...)
    vs, s = [], 0
    for n in ns
-     v = view(u, (1:n) .+ s)
-     s += n
+     N = prod(n)
+     v = reshape(view(u, (1:N) .+ s), n...)
+     s += N
      push!(vs, v)
    end    
    vs   
